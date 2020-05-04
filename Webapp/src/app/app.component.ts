abcb45e3fa5d43ff0cff4741c48e6ec20be7f926
unknown <jon.a.havener@gmail.com>
2020-05-04 01:17:35 -0400

    Added main page carousel affect

--- Webapp/src/app/app.component.ts
+++ Webapp/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webapp';
+
+  itemsPerSlide = 3;
+  singleSlideOffset = true;
+  noWrap = true;
+ 
+  slidesChangeMessage = '';
+ 
+  slides = [
+    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/1.jpg'},
+    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/2.jpg'},
+    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/3.jpg'},
+    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/4.jpg'},
+    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/5.jpg'},
+    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/6.jpg'},
+    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/7.jpg'},
+    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/8.jpg'},
+    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/1.jpg'},
+    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/2.jpg'}
+  ];
+ 
+  onSlideRangeChange(indexes: number[]): void {
+    this.slidesChangeMessage = `Slides have been switched: ${indexes}`;
+  }
 }
--- Webapp/src/app/app.module.ts
+++ Webapp/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
